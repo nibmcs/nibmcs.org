103bb433fa4d3910d699bc7b99f41380417aadff
Tharindu Jayasanka <hello@tharindu.me>
2023-10-12 21:50:18 +0530

    feat: Next UI integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Providers } from './providers';
 
 export const metadata: Metadata = {
   title: "NIBM Computing Society",
@@ -16,8 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en' className='dark'>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
