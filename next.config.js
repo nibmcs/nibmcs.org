7ab581a59b5db2d31cea152c7bdeb951f7cd1526
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2023-10-30 01:43:35 +0530

    chore: Update next.js config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+//   distDir: 'build',
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  distDir: 'build',
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
