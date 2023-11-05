d33b40b0c02241ebafb6f6d88696eb3a7693430c
Chamidu Chamodya <47558333+ChamiduChamodya@users.noreply.github.com>
2023-11-05 22:12:49 +0530

    feat(Events): Mobile Responsive

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -30,7 +30,7 @@ export default function EventsPage() {
     <div className="container mx-auto px-4">
       <div className="flex flex-wrap mx-4">
         {events.map((event, index) => (
-          <div key={index} className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/3 xl:w-1/3 2xl:w-1/3 px-3">
+          <div key={index} className="w-full sm:w-0 md:w-1/2 lg:w-1/2 xl:w-1/3 2xl:w-1/3 px-3">
             <EventsCard />
           </div>
         ))}
