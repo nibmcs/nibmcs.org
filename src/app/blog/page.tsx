a262c3613fbe97681b2f7355ec916cfa640d0aa7
Savindu Supun <savindusupunsathsara@gmail.com>
2023-11-07 22:27:21 +0530

    feat(Blogs): Redesign the blog page layout

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import {FaPenNib} from 'react-icons/fa';
+import { FaPenNib } from 'react-icons/fa';
 interface BlogPost {
   title: string;
   author: string;
   pubDate: string;
   link: string;
-  cover_image?: string;
+  cover_image: string;
   description: string;
 }
 
@@ -22,44 +22,73 @@ async function getBlogPosts() {
 
 export default async function BlogPage() {
   const blogs: BlogPost[] = await getBlogPosts();
+  const heroBlog: BlogPost = blogs[0];
+  const moreBlogs: BlogPost[] = blogs.slice(1);
   return (
-    <div className="w-full">
+    <div className="max-w-5xl text-left">
       <h1 className="text-center text-4xl font-bold">Blog</h1>
-      <div className="flex flex-col w-full gap-10 text-left mt-10">
-        {blogs.map((blog, index) => (
-          <div className="flex md:flex-row flex-col gap-4" key={`blog-${index}`}>
-            <Image
-              src={blog.cover_image || 'https://cdn.hashnode.com/res/hashnode/image/upload/v1679075289714/I2spUyUW6.png'}
-              width={1869}
-              height={750}
-              alt={blog.title}
-              className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
-            />
-            <div className="w-full flex justify-between items-center">
-              <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
-                <h3 className="font-normal lg:text-[35px] text-[26px]">
+      <div className="mb-8 md:mb-16">
+        <div className="relative h-80 my-10">
+          <Image
+            src={heroBlog.cover_image}
+            alt={heroBlog.title}
+            layout="fill"
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+          <div className="text-left">
+            <h3 className="mb-3 text-4xl lg:text-6xl leading-tight">
+              <a href={heroBlog.link} className="hover:underline">
+                {heroBlog.title}
+              </a>
+            </h3>
+            <div className="mb-3 md:mb-0 text-lg font-light">
+                {new Date(heroBlog.pubDate).toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' })}
+            </div>
+          </div>
+          <div className="text-left">
+            <p className="text-md font-extralight leading-relaxed mb-3">{heroBlog.description}</p>
+            <div className="text-md">
+              <FaPenNib className="inline-block mr-2" />
+              {heroBlog.author}
+            </div>
+          </div>
+        </div>
+      </div>
+      <h2 className="text-3xl font-bold mb-4">More Posts</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-14">
+        {moreBlogs.map((blog) => (
+          <div key={blog.title} className="flex flex-col">
+            <div className="relative h-64">
+              <Image
+                src={blog.cover_image}
+                alt={blog.title}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-lg"
+              />
+            </div>
+            <div className="flex flex-col flex-grow gap-0">
+              <h3 className="text-xl font-bold my-2">
+                <a href={blog.link} className="hover:underline">
                   {blog.title}
-                </h3>
-                <div className="flex items-center mt-2">
-                  <FaPenNib className="text-gray-700 dark:text-gray-300" />
-                  <p className="text-[14px] ml-[8px] text-gray-700 dark:text-gray-300">
-                    {blog.author}
-                  </p>
-                </div>
-                <p className="mt-[16px] font-normal lg:text-[20px] text-[14px]">
-                  {blog.description}
-                </p>
-                <a href={blog.link} title={blog.title} target="_blank" rel="noreferrer">
-                  <p className="text-[15px] pt-1 underline">
-                    Read more <span className="sr-only">about {blog.title}</span>
-                  </p>
                 </a>
+              </h3>
+              <div className="mb-2 text-sm font-light">
+              {new Date(heroBlog.pubDate).toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' })}
+              </div>
+              <div className="text-md font-extralight leading-relaxed mb-2">
+                {blog.description}
+              </div>
+              <div className="text-sm">
+                <FaPenNib className="inline-block mr-2" />
+                {blog.author}
               </div>
             </div>
           </div>
         ))}
-
-      </div>
+        </div>
     </div>
   );
 }
